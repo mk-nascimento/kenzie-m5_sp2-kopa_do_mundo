abbd1384ebb45e13fc671bea5de8658171f8240b
Alexandre Alves <alexandre@kenzie.com.br>
2023-05-19 16:39:32 -0300

    Ajuste de nomenclatura das tarefas e do README

--- README.md
+++ README.md
@@ -40,16 +40,16 @@ pip install pytest-testdox pytest-django
 
 Exemplo:
 
-- Tarefa 0
+- Tarefa 1
 
 ```shell
-pytest --testdox -vvs tests/tarefas/tarefa_0/
+pytest --testdox -vvs tests/tarefas/tarefa_1/
 ```
 
 ---
 
-## Execução de testes a partir da tarefa 1
-A partir de agora, para os testes das tarefas 1, 2 e 3, já que começaremos a usar o Django, precisaremos de um arquivo **pytest.ini**, você **DEVE** cria-lo na raiz do projeto, depois de criar esse aquivo você precisa adicionar nele a seguinte configuração:
+## Execução de testes a partir da tarefa 2
+A partir de agora, para os testes das tarefas 2, 3 e 4, já que começaremos a usar o Django, precisaremos de um arquivo **pytest.ini**, você **DEVE** cria-lo na raiz do projeto, depois de criar esse aquivo você precisa adicionar nele a seguinte configuração:
 
 ```python
 [pytest]
@@ -58,18 +58,12 @@ DJANGO_SETTINGS_MODULE = <nome_do_projeto>.settings
 
 **IMPORTANTE**:  Troque <nome_do_projeto> para o nome do pacote onde fica o arquivo settings.py do projeto Django.
 
-- Tarefa 0
-
-
-```shell
-pytest --testdox -vvs tests/tarefas/tarefa_0/
-```
-
 - Tarefa 1
 
 ```shell
 pytest --testdox -vvs tests/tarefas/tarefa_1/
 ```
+
 - Tarefa 2
 
 ```shell
@@ -80,6 +74,12 @@ pytest --testdox -vvs tests/tarefas/tarefa_2/
 
 ```shell
 pytest --testdox -vvs tests/tarefas/tarefa_3/
+```
+
+- Tarefa 4
+
+```shell
+pytest --testdox -vvs tests/tarefas/tarefa_4/
 ```
 
 ---
--- tests/tarefas/tarefa_3/test_views.py
+++ tests/tarefas/tarefa_3/test_views.py
@@ -1,96 +1,213 @@
 from rest_framework.test import APITestCase
 from teams.models import Team
 from rest_framework.views import status
-from datetime import date
 
 
-class TeamViewsTest(APITestCase):
+class CreateTeamTest(APITestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         cls.BASE_URL = "/api/teams/"
-        cls.BASE_DETAIL_URL = cls.BASE_URL + "1/"
 
-    def test_if_a_team_can_be_updated(self):
-        team_1_data = {
+    def test_if_a_team_can_be_created(self):
+        team_data = {
             "name": "Brasil",
             "titles": 5,
             "top_scorer": "Pelé",
             "fifa_code": "BRA",
             "first_cup": "1930-07-13",
         }
-        # Criando time 1
-        Team.objects.create(**team_1_data)
 
-        team_1_patch_data = {
-            "name": "Brasil 5000",
-            "top_scorer": "Alejo",
+        # REQUEST
+        response = self.client.post(self.BASE_URL, data=team_data, format="json")
+
+        # EXPECTED JSON RETURN
+        expected_data = {
+            "id": 1,
+            "name": "Brasil",
+            "titles": 5,
+            "top_scorer": "Pelé",
+            "fifa_code": "BRA",
+            "first_cup": "1930-07-13",
         }
-        response = self.client.patch(
-            self.BASE_DETAIL_URL, data=team_1_patch_data, format="json"
-        )
-
-        expected_status_code = status.HTTP_200_OK
-        result_status_code = response.status_code
-        msg = (
-            "Verifique se o status code retornado do PATCH "
-            + f"em `{self.BASE_URL}` é {expected_status_code}"
-        )
-        self.assertEqual(expected_status_code, result_status_code, msg)
-
-        expected_data = {**team_1_data, **team_1_patch_data, "id": 1}
         result_data = response.json()
-        msg = "Verifique se as informações da seleçao atualizada foi retornada corretamente"
+        msg = (
+            "Verifique as informações da seleção retornada no POST "
+            + f"em `{self.BASE_URL}` estão corretas."
+        )
         self.assertEqual(expected_data, result_data, msg)
 
-        team = Team.objects.get(id=1)
-        for key, value in expected_data.items():
-            obj_value = getattr(team, key)
-            if isinstance(obj_value, date):
-                obj_value = obj_value.strftime("%Y-%m-%d")
-            msg = f"Verifique se as alterações no campo `{key}` foram persistidas no banco"
-            self.assertEqual(value, obj_value, msg)
-
-    def test_if_a_team_can_be_deleted(self):
-        team_1_data = {
-            "name": "Brasil",
-            "titles": 5,
-            "top_scorer": "Pelé",
-            "fifa_code": "BRA",
-            "first_cup": "1930-07-13",
-        }
-        # Criando time 1
-        Team.objects.create(**team_1_data)
-
-        response = self.client.delete(self.BASE_DETAIL_URL)
-
-        expected_status_code = status.HTTP_204_NO_CONTENT
+        # STATUS CODE
+        expected_status_code = status.HTTP_201_CREATED
         result_status_code = response.status_code
         msg = (
-            "Verifique se o status code retornado do DELETE "
+            "Verifique se o status code retornado do POST "
             + f"em `{self.BASE_URL}` é {expected_status_code}"
         )
         self.assertEqual(expected_status_code, result_status_code, msg)
 
-        msg = "Verifique se a deleção não está retornando nenhum body"
-        self.assertRaises(TypeError, response.json)
-
-        msg = "Verifique se o registro está sendo deletado do banco corretamente"
-        self.assertFalse(Team.objects.exists(), msg)
-
-    def test_if_a_team_can_be_retrieved(self):
-        team_1_data = {
+    def test_if_a_team_cannot_have_negative_titles(self):
+        team_data = {
             "name": "Brasil",
-            "titles": 5,
+            "titles": -5,
             "top_scorer": "Pelé",
             "fifa_code": "BRA",
             "first_cup": "1930-07-13",
         }
 
+        # REQUEST
+        response = self.client.post(self.BASE_URL, data=team_data, format="json")
+
+        # EXPECTED JSON RETURN
+        expected_data = {"error": "titles cannot be negative"}
+        result_data = response.json()
+        msg = (
+            f"Verifique se a mensagem de erro retornada do POST em `{self.BASE_URL}` "
+            + f"com `titles` negativo é `{expected_data}`"
+        )
+        self.assertEqual(expected_data, result_data, msg)
+
+        # EXPECTED STATUS CODE
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        result_status_code = response.status_code
+        msg = (
+            f"Verifique se o status code retornado do POST em `{self.BASE_URL}` "
+            + f"com `titles` negativo é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+    def test_if_first_cup_is_a_valid_world_cup_year(self):
+        team_data = {
+            "name": "Brasil",
+            "titles": 5,
+            "top_scorer": "Pelé",
+            "fifa_code": "BRA",
+            "first_cup": "2003-08-18",
+        }
+
+        # REQUEST
+        response = self.client.post(self.BASE_URL, data=team_data, format="json")
+
+        # EXPECTED JSON RETURN
+        expected_data = {"error": "there was no world cup this year"}
+        result_data = response.json()
+        msg = (
+            f"Verifique se a mensagem de erro retornada do POST em `{self.BASE_URL}` "
+            + f"com `first_cup` nao sendo um ano de copa válido pós 1930 é `{expected_data}`"
+        )
+        self.assertEqual(expected_data, result_data, msg)
+
+        # EXPECTED STATUS CODE
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        result_status_code = response.status_code
+        msg = (
+            f"Verifique se o status code retornado do POST em `{self.BASE_URL}` "
+            + f"com `first_cup` nao sendo um ano de copa válido pós 1930 é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+    def test_if_first_cup_cannot_be_before_1930(self):
+        team_data = {
+            "name": "Brasil",
+            "titles": 5,
+            "top_scorer": "Pelé",
+            "fifa_code": "BRA",
+            "first_cup": "1926-08-18",
+        }
+
+        # REQUEST
+        response = self.client.post(self.BASE_URL, data=team_data, format="json")
+
+        # EXPECTED JSON RETURN
+        expected_data = {"error": "there was no world cup this year"}
+        result_data = response.json()
+        msg = (
+            f"Verifique se a mensagem de erro retornada do POST em `{self.BASE_URL}` "
+            + f"com `first_cup` anterior a 1930 é `{expected_data}`"
+        )
+        self.assertEqual(expected_data, result_data, msg)
+
+        # EXPECTED STATUS CODE
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        result_status_code = response.status_code
+        msg = (
+            f"Verifique se o status code retornado do POST em `{self.BASE_URL}` "
+            + f"com `first_cup` anterior a 1930 é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+    def test_if_title_number_is_possible_based_on_first_cup(self):
+        team_data = {
+            "name": "Brasil",
+            "titles": 4,
+            "top_scorer": "Pelé",
+            "fifa_code": "BRA",
+            "first_cup": "2022-08-18",
+        }
+
+        # REQUEST
+        response = self.client.post(self.BASE_URL, data=team_data, format="json")
+
+        # EXPECTED JSON RETURN
+        expected_data = {"error": "impossible to have more titles than disputed cups"}
+        result_data = response.json()
+        msg = (
+            f"Verifique se a mensagem de erro retornada do POST em `{self.BASE_URL}` "
+            + f"com `titles` sendo impossivel baseado em `first_cup` é `{expected_data}`"
+        )
+        self.assertEqual(expected_data, result_data, msg)
+
+        # EXPECTED STATUS CODE
+        expected_status_code = status.HTTP_400_BAD_REQUEST
+        result_status_code = response.status_code
+        msg = (
+            f"Verifique se o status code retornado do POST em `{self.BASE_URL}` "
+            + f"com `titles` sendo impossivel baseado em `first_cup` é {expected_status_code}"
+        )
+        self.assertEqual(expected_status_code, result_status_code, msg)
+
+
+class ListTeamTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.BASE_URL = "/api/teams/"
+
+    def test_if_teams_can_be_listed(self):
+        team_1_data = {
+            "name": "Brasil",
+            "titles": 5,
+            "top_scorer": "Pelé",
+            "fifa_code": "BRA",
+            "first_cup": "1930-06-08",
+        }
+        team_2_data = {
+            "name": "Argentina",
+            "titles": 2,
+            "top_scorer": "Lionel Messi",
+            "fifa_code": "ARG",
+            "first_cup": "1934-02-21",
+        }
+
         # Criando time 1
         Team.objects.create(**team_1_data)
+        # Criando time 2
+        Team.objects.create(**team_2_data)
 
-        response = self.client.get(self.BASE_DETAIL_URL)
+        # REQUEST
+        response = self.client.get(self.BASE_URL)
 
+        team_1_return = {**team_1_data, "id": 1}
+        team_2_return = {**team_2_data, "id": 2}
+
+        # EXPECTED JSON RETURN
+        expected_data = [team_1_return, team_2_return]
+        result_data = response.json()
+        msg = (
+            "Verifique se as informações das seleções listadas no GET "
+            + f"em `{self.BASE_URL}` estão corretas."
+        )
+        self.assertEqual(expected_data, result_data, msg)
+
+        # EXPECTED STATUS CODE
         expected_status_code = status.HTTP_200_OK
         result_status_code = response.status_code
         msg = (
@@ -98,63 +215,3 @@ class TeamViewsTest(APITestCase):
             + f"em `{self.BASE_URL}` é {expected_status_code}"
         )
         self.assertEqual(expected_status_code, result_status_code, msg)
-
-        expected_data = {**team_1_data, "id": 1}
-
-        result_data = response.json()
-        msg = (
-            "Verifique as informações da seleção retornada no GET "
-            + f"em `{self.BASE_URL}` estão corretas."
-        )
-        self.assertEqual(expected_data, result_data, msg)
-
-    def test_non_existing_id_delete(self):
-        non_existing_id_url = self.BASE_URL + "12234/"
-        response = self.client.delete(non_existing_id_url)
-
-        expected_status_code = status.HTTP_404_NOT_FOUND
-        result_status_code = response.status_code
-        msg = (
-            "Verifique se o status code retornado do DELETE com id inválido"
-            + f"em `{non_existing_id_url}` é {expected_status_code}"
-        )
-        self.assertEqual(expected_status_code, result_status_code, msg)
-
-        expected = {"message": "Team not found"}
-        result = response.json()
-        msg = "Verifique se a mensagem de DELETE com id inválido está correta"
-        self.assertDictEqual(expected, result, msg)
-
-    def test_non_existing_id_update(self):
-        non_existing_id_url = self.BASE_URL + "12234/"
-        response = self.client.patch(non_existing_id_url)
-
-        expected_status_code = status.HTTP_404_NOT_FOUND
-        result_status_code = response.status_code
-        msg = (
-            "Verifique se o status code retornado do PATCH com id inválido"
-            + f"em `{non_existing_id_url}` é {expected_status_code}"
-        )
-        self.assertEqual(expected_status_code, result_status_code, msg)
-
-        expected = {"message": "Team not found"}
-        result = response.json()
-        msg = "Verifique se a mensagem de PATCH com id inválido está correta"
-        self.assertDictEqual(expected, result, msg)
-
-    def test_non_existing_id_retrieve(self):
-        non_existing_id_url = self.BASE_URL + "12234/"
-        response = self.client.get(non_existing_id_url)
-
-        expected_status_code = status.HTTP_404_NOT_FOUND
-        result_status_code = response.status_code
-        msg = (
-            "Verifique se o status code retornado do GET com id inválido"
-            + f"em `{non_existing_id_url}` é {expected_status_code}"
-        )
-        self.assertEqual(expected_status_code, result_status_code, msg)
-
-        expected = {"message": "Team not found"}
-        result = response.json()
-        msg = "Verifique se a mensagem de GET com id inválido está correta"
-        self.assertDictEqual(expected, result, msg)
